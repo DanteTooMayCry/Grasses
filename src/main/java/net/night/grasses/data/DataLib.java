981abf509c8553b96b521ab1c5f28113ebb3ffe2
DanteTooMayCry <dantetoomaycry@gmail.com>
2025-07-05 11:15:31 +0200

    test

--- src/main/java/net/night/grasses/data/DataLib.java
+++ src/main/java/net/night/grasses/data/DataLib.java
@@ -32,6 +32,7 @@ public class DataLib {
     public static final List<ColorType> colorTypeList = new ArrayList<>();
 
     public static final Map<String, Block> counterpartIDMap = new HashMap<>();
+    public static final Map<String, Block> testtest = new HashMap<>();
 
     public static final Map<Block, Block> matchingCounterpartsLeaves = new HashMap<>();
     public static final Map<Block, Block> matchingCounterpartsPlants = new HashMap<>();
