cce7a4cc9cc6b1e5aecfc0471902bbbd617b4df2
DanteTooMayCry <dantetoomaycry@gmail.com>
2025-09-09 14:02:59 +0200

    Recommit

--- Forge/Forge 1.20.1/src/main/java/net/night/grasses/config/additionalDropSystem/ConfigDrops.java
+++
@@ -1,8 +0,0 @@
-package net.night.grasses.config.additionalDropSystem;
-
-import java.util.Map;
-
-public record ConfigDrops(
-        Map<BlockCondition, AdditionalDropConfig.DropGroup> blockDropMap,
-        Map<MobCondition, AdditionalDropConfig.DropGroup> mobDropMap) {
-}
